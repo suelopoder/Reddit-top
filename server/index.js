0b30bd2db740c42f115d5ba1aa83e6ef594f8146
Diego Sisto <diego.sisto@xapo.com>
2020-05-22 12:28:32 -0300

    Completed POC of reddit oauth login

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,40 @@ if (!redditAppId) {
   console.error('Please setup a ".env" file based on ".env.example" to run this app');
   process.exit(1);
 }
+
+app.get('/reddit_redirect', async (req, res) => {
+  // TODO validate state matches REDDIT_TEST_CLIENT for security
+  const code = req.query.code;
+  if (!code) {
+    return res.status(400).send('No code submitted');
+  }
+
+  try {
+    const { REDDIT_APP_ID, REDDIT_APP_SECRET, REDDIT_APP_REDIRECT_URI } = process.env;
+    const response = await axios.post(
+      'https://www.reddit.com/api/v1/access_token',
+      `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(REDDIT_APP_REDIRECT_URI)}`,
+      {
+        headers: {
+          'User-Agent': 'REDDIT_TEST_CLIENT'
+        },
+        auth: {
+          username: REDDIT_APP_ID,
+          password: REDDIT_APP_SECRET
+        }
+      });
+
+    const { error } = response.data;
+    if (error) {
+      console.error('Error response getting access token', error);
+      return res.status(400).send('Invalid code');
+    }
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Invalid code');
+  }
+});
 
 const PORT = process.env.SERVER_PORT || 3001;
 app.listen(PORT, function () {
--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Here will go the app
-    </div>
-  );
+const REDDIT_TOKEN_KEY = 'REDDIT_CLIENT__REDDIT_TOKEN_KEY';
+const getRedditToken = () => window.localStorage.getItem(REDDIT_TOKEN_KEY);
+
+const REDDIT_APP_ID = process.env.REACT_APP_REDDIT_APP_ID;
+const REDDIT_REDIRECT_URI = encodeURIComponent(process.env.REACT_APP_REDDIT_APP_REDIRECT_URI);
+const REDDIT_AUTH = `https://www.reddit.com/api/v1/authorize?client_id=${REDDIT_APP_ID}&response_type=code&state=REDDIT_TEST_CLIENT&redirect_uri=${REDDIT_REDIRECT_URI}&duration=temporary&scope=read+identity+modmail`;
+const OAUTH_URL = `https://www.reddit.com/login/?dest=${encodeURIComponent(REDDIT_AUTH)}`;
+
+class App extends Component {
+  componentDidMount() {
+    const token = getRedditToken();
+    if (!token) {
+      window.location.replace(OAUTH_URL);
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        Here will go the app
+      </div>
+    );
+  }
 }
 
 export default App;
