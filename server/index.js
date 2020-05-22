6d339fb8fa247203534b92db970dd4b935348e47
Diego Sisto <diego.sisto@xapo.com>
2020-05-22 10:29:00 -0300

    Add .env and make server fault if config is missing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 // server react build
 app.use(express.static('build'));
+
+const redditAppId = process.env.REDDIT_APP_ID;
+if (!redditAppId) {
+  console.error('Please setup a ".env" file based on ".env.example" to run this app');
+  process.exit(1);
+}
 
 const PORT = process.env.SERVER_PORT || 3001;
 app.listen(PORT, function () {
