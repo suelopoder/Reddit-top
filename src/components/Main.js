6b88ab6c4f815c6493db0a06c15f252a602ce994
Diego Sisto <diego.sisto@xapo.com>
2020-05-25 13:39:31 -0300

    Refactor out UI from logic. Fix nav scroll

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,17 @@ import PostList from './PostList';
 import PostDetail from './PostDetail';
 
 const MainWrapper = styled.div`
-  display: flex;
+  > *:nth-child(1) {
+    position: fixed;
+    width: 350px;
+    overflow-y: scroll;
+    top: 0;
+    bottom: 0;
+  }
+  > *:nth-child(2) {
+    position: relative;
+    margin-left: 350px;
+  }
 `;
 
 export default function Main({ posts }) {
