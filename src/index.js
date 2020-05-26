3500b3635cd0c91ef73d1fca739b14873f6313f1
Diego Sisto <diego.sisto@xapo.com>
2020-05-26 12:31:05 -0300

    Remove validation to add redux devtools in prod

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import App from './App';
 import rootReducer from './reducers';
 import redditDataMiddleware from './redditDataMiddleware';
 
-const isDevelopment = () => process.env.NODE_ENV === 'development';
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(redditDataMiddleware),
-    isDevelopment() && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
